d721df38f9b3d02a532c56fa5388b8874f112291
ben <benjamin.j.correia@gmail.com>
2020-03-19 04:08:03 -0400

    Implementation no testing

--- CameraMath.cpp
+++ CameraMath.cpp
@@ -1,36 +1,79 @@
 #include "CameraMath.h"
 #include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif // !_USE_MATH_DEFINES
+
+#include <math.h>
 
 
-static double imHeight, imWidth, f, sensorW, sensorH, balloonSpan;
+static double
+	imHeight, //pixels
+	imWidth, //pixels
+	f, // mm
+	sensorW, //mm
+	sensorH, //mm
+	balloonSpan, //mm
+	sigma; //mm/px pixel size
+
+static CameraMath::pos getCoordinates(double pxSize, double pxX, double pxY) {
+	double z = CameraMath::calcDistance(pxSize);
+	double x = sigma * pxX * z / f;
+	double y = sigma * pxY * z / f;
+	return { x,y,z };
+}
 
 void CameraMath::useSettings(SettingsWrapper & sw, int imageHeight, int imageWidth, double balloonCircumference) {
+	imHeight = imageHeight;
+	imWidth = imageWidth;
+	f = sw.focal_length_min;
+	sensorW = sw.sensor_width;
+	sensorH = sw.sensor_height;
+	balloonSpan = 10.0 * balloonCircumference * M_1_PI;
+	sigma = sensorW / imWidth;
 }
 
-double CameraMath::calcRadius(double area) {
-	return 0.0;
+inline double CameraMath::calcDiameter(double area) {
+	return sqrt(area * M_1_PI);
 }
 
-double CameraMath::calcDistance(double pxSize) {
-	return 0.0;
+inline double CameraMath::calcDistance(double pxSize) {
+	return balloonSpan * f / (pxSize * sigma); // mm
 }
 
 double CameraMath::calcRadialDistance(double pxSize, double pxX, double pxY) {
-	return 0.0;
+	auto r = getCoordinates(pxSize, pxX, pxY);
+	return sqrt(r.z*r.z + r.x*r.x + r.y*r.y);
 }
 
-double CameraMath::calcPanRelative(double pxX) {
-	return 0.0;
+inline double CameraMath::calcPanRelative(double pxX) {
+	return atan(sigma * pxX / f) * M_1_PI * 180.0;
 }
 
-double CameraMath::calcTiltRelative(double pxY) {
-	return 0.0;
+inline double CameraMath::calcTiltRelative(double pxY) {
+	return atan(sigma * pxY / f) * M_1_PI * 180.0;
 }
 
 CameraMath::pos CameraMath::calcRelativePos(double pan, double tilt, double radial) {
-	return pos();
+	pos returner;
+
+	//Forward : +x
+	//Left : +y
+	//Up : +z
+	returner.z = radial * sin(tilt * M_PI / 180.0);
+	returner.x = radial * cos(tilt * M_PI / 180.0) * cos(pan * M_PI / 180.0);
+	returner.y = radial * cos(tilt * M_PI / 180.0) * sin(pan * M_PI / 180.0);
+
+	return returner;
 }
 
-CameraMath::pos CameraMath::calcRelativePos(double pxSize, double pxX, double pxY) {
-	return pos();
+CameraMath::pos CameraMath::calcRelativePos(double area, double pxX, double pxY, double pan, double tilt) {
+	double pxSize = CameraMath::calcDiameter(area);
+	pan += CameraMath::calcPanRelative(pxX);
+	tilt += CameraMath::calcTiltRelative(pxY);
+	double r = CameraMath::calcRadialDistance(pxSize, pxX, pxY);
+	return CameraMath::calcRelativePos(pan, tilt, r);
 }
--- CameraMath.h
+++ CameraMath.h
@@ -3,13 +3,55 @@
 #include "SettingsWrapper.h"
 
 namespace CameraMath {
+	/* Initialize camera math calculations
+	sw : loaded from the settigs file
+	imageHeight : number of rows in the image
+	image width : number of cols in the image
+	balloonCircumference : real world circumference of the balloon in centimeters
+	*/
 	void useSettings(SettingsWrapper &sw, int imageHeight, int imageWidth, double balloonCircumference);
-	double calcRadius(double area);
+
+	/* Calculate the radius of the balloon in pixels
+	area : visual area of the balloon in pixels	
+	*/
+	double calcDiameter(double area);
+
+	/* Calculate the distance from the focal point in the direction of the normal of the sensing plane (millimeters)
+	pxSize : the visual diameter of the balloon
+	*/
 	double calcDistance(double pxSize);
+
+	/* Calculate the radial distance from the focal point (millimeters)
+	pxSize : the visual diameter of the balloon
+	pxX : the column of the mean pixel of the balloon
+	pxY : the row of the mean pixel of the balloon
+	*/
 	double calcRadialDistance(double pxSize, double pxX, double pxY);
+
+	/* Calculate the pan rotation of the balloon relative to the camera
+	pxX : the column of the mean pixel of the balloon
+	*/
 	double calcPanRelative(double pxX);
+
+	/* Calculate the tilt rotation of the balloon relative to the camera
+	pxY : the row of the mean pixel of the balloon
+	*/
 	double calcTiltRelative(double pxY);
 	struct pos { double x, y, z; };
+
+	/* Convert from a spherical coordinate system to cartesian coordinate system
+	pan : degrees
+	tilt : degrees
+	radial : millimeters
+	*/
 	pos calcRelativePos(double pan, double tilt, double radial);
-	pos calcRelativePos(double pxSize, double pxX, double pxY);
+
+	/* Calculate the spherical coordinates of the balloon given its size and position
+	area : visual area of the balloon in pixels
+	pxX : the column of the mean pixel of the balloon
+	pxY : the row of the mean pixel of the balloon
+	pan : pan in degrees
+	tilt : tilt in degrees
+	*/
+	pos calcRelativePos(double area, double pxX, double pxY, double pan, double tilt);
 }
