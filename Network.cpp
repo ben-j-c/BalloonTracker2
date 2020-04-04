6f619d9a7125305b4c465b7272cda8defdfe7d30
ben <benjamin.j.correia@gmail.com>
2020-04-04 07:32:11 -0400

    Running hour test.

--- Network.cpp
+++ Network.cpp
@@ -33,66 +33,71 @@ void Network::useSettings(SettingsWrapper & sw) {
 Returns -1 on error, 0 otherwise.
 */
 int Network::startServer() {
-	if (socketEnabled) {
-		WSADATA wsaData;
-		int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (err) {
-			cerr << "WSAStartup failed: " << err << endl;
-			return -1;
-		}
-
-		struct addrinfo *result = NULL, *ptr = NULL, hints;
-		ZeroMemory(&hints, sizeof(hints));
-		hints.ai_family = AF_INET;
-		hints.ai_socktype = SOCK_STREAM;
-		hints.ai_protocol = IPPROTO_TCP;
-		hints.ai_flags = AI_PASSIVE;
-
-		{
-			err = getaddrinfo(NULL, std::to_string(port).data(), &hints, &result);
-			if (err) {
-				WSACleanup();
-				cerr << "getaddrinfo failed: " << err << endl;
-				return -1;
-			}
-		}
-
-		server = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-
-		if (server == INVALID_SOCKET) {
-			cerr << "socket failed: " << WSAGetLastError() << endl;
-			freeaddrinfo(result);
-			WSACleanup();
-			return -1;
-		}
-
-		err = bind(server, result->ai_addr, (int)result->ai_addrlen);
-		if (err == SOCKET_ERROR) {
-			cerr << "bind failed: " << WSAGetLastError() << endl;
-			server = INVALID_SOCKET;
-			closesocket(server);
-			server = INVALID_SOCKET;
-			freeaddrinfo(result);
-			WSACleanup();
-			return -1;
-		}
-
-		freeaddrinfo(result);
-
-		if (listen(server, SOMAXCONN) == SOCKET_ERROR) {
-			cerr << "listen failed: " << WSAGetLastError() << endl;
-			closesocket(server);
-			server = INVALID_SOCKET;
-			WSACleanup();
-			return -1;
-		}
-
-		return 0;
-	}
-	else {
+	if (!socketEnabled) {
 		cerr << CERR_NOT_ENABLED;
 		return -1;
 	}
+
+	if (server != INVALID_SOCKET) {
+		cerr << "Server already started" << endl;
+		return -1;
+	}	
+
+	WSADATA wsaData;
+	int err = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (err) {
+		cerr << "WSAStartup failed: " << err << endl;
+		return -1;
+	}
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	{
+		err = getaddrinfo(NULL, std::to_string(port).data(), &hints, &result);
+		if (err) {
+			WSACleanup();
+			cerr << "getaddrinfo failed: " << err << endl;
+			return -1;
+		}
+	}
+
+	server = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+	if (server == INVALID_SOCKET) {
+		cerr << "socket failed: " << WSAGetLastError() << endl;
+		freeaddrinfo(result);
+		WSACleanup();
+		return -1;
+	}
+
+	err = bind(server, result->ai_addr, (int)result->ai_addrlen);
+	if (err == SOCKET_ERROR) {
+		cerr << "bind failed: " << WSAGetLastError() << endl;
+		server = INVALID_SOCKET;
+		closesocket(server);
+		server = INVALID_SOCKET;
+		freeaddrinfo(result);
+		WSACleanup();
+		return -1;
+	}
+
+	freeaddrinfo(result);
+
+	if (listen(server, SOMAXCONN) == SOCKET_ERROR) {
+		cerr << "listen failed: " << WSAGetLastError() << endl;
+		closesocket(server);
+		server = INVALID_SOCKET;
+		WSACleanup();
+		return -1;
+	}
+
+	return 0;
+
 }
 
 /* Accept client connection.
@@ -176,7 +181,7 @@ std::vector<char> Network::recvData() {
 		return std::vector<char>();
 	}
 
-	if ((int64_t) nBytes > (int64_t) nBytesActual) {
+	if ((int64_t)nBytes > (int64_t)nBytesActual) {
 		cout << "Received more bytes durring read." << endl;
 	}
 	return returner;
--- Test_Network_1Hour/Test_Network_1Hour.cpp
+++ Test_Network_1Hour/Test_Network_1Hour.cpp
@@ -15,8 +15,9 @@ int main() {
 	SettingsWrapper sw("../settings.json");
 	Network::useSettings(sw);
 	Network::startServer();
-	Network::startServer();
+	std::cout << "Server started, waiting for client" << std::endl;
 	Network::acceptConnection();
+	std::cout << "Client connected" << std::endl;
 
 	std::vector<double> data(25 * 3 * 3); //25 FPS, 3 doubles per frame, 3 seconds
 	for (int i = 0; i < data.size(); i++) {
@@ -25,9 +26,11 @@ int main() {
 	std::cout << "Sending 3 second frame pack.";
 
 	for (int i = 0; i < 1200; i++) {
-		backspace(100);
+		backspace(1000);
 		std::cout << "Sending 3 second frame pack. " << i << "/" << "1200";
-		Network::sendData(data);
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		if (Network::sendData(data) == -1)
+			return -1;
+		std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 	}
+	return 0;
 }
