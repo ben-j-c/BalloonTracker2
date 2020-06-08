f7a5dca3eabc72f0648f06e9b402b3e3dc13487b
Ben C <benjamin.j.correia@gmail.com>
2020-06-08 18:33:29 -0400

    Added interface.

--- include/BalloonTrackerGUI.h
+++ include/BalloonTrackerGUI.h
@@ -1,6 +1,15 @@
 #pragma once
+#include "SettingsWrapper.h"
+#include <vector>
 
 namespace GUI {
+	struct DataPoint{
+		double x, y, z;
+		double mPan, mTilt, pPan, pTilt;
+		uint64_t index;
+	};
+	extern std::vector<DataPoint> data;
+
 	extern bool bStartSystemRequest;
 	extern bool bStopSystemRequest;
 	extern bool bStartImageProcRequest;
@@ -11,4 +20,6 @@ namespace GUI {
 	extern double dCountDown;
 	extern double dCountDownValue;
 	extern float fBearing;
+
+	int StartGUI(SettingsWrapper& sw);
 }
