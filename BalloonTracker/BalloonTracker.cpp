1fa1f085bb50393c145edf38614bdce823621904
Ben C <benjamin.j.correia@gmail.com>
2020-11-25 00:23:53 -0500

    Drop in new image acq.

--- BalloonTracker/BalloonTracker.cpp
+++ BalloonTracker/BalloonTracker.cpp
@@ -10,6 +10,7 @@
 #include <CameraMath.h>
 #include <BalloonTrackerGUI.h>
 #include <rapidjson/document.h>
+#include <VideoReader.h>
 
 // C/C++
 #include <vector>
@@ -37,6 +38,7 @@
 #include <opencv2/cudaimgproc.hpp>
 #include <opencv2/cudawarping.hpp>
 
+#define USE_VIDEOREADER_BEN
 
 using namespace cv;
 using namespace std;
@@ -314,6 +316,60 @@ void processFrames() {
 	}
 }
 
+#ifdef USE_VIDEOREADER_BEN
+
+void consumeBufferedFrames(VideoReader& vid, ImageRes& buff) {
+	using milisec = std::chrono::duration<float, std::milli>;
+	std::chrono::high_resolution_clock timer;
+	auto start = timer.now();
+	vid.readFrame(buff);
+	auto stop = timer.now();
+	auto dt = std::chrono::duration_cast<milisec>(stop - start);
+
+	//We need to drop all the frames we wont use.
+	//We know that the frames pile up before we are able to process them.
+	cout << "INFO: processVideo consuming frames." << endl;
+	while (dt.count() < 1000.0f / 30) {
+		auto start = timer.now();
+		vid.readFrame(buff);
+		auto stop = timer.now();
+		dt = std::chrono::duration_cast<milisec>(stop - start);
+	}
+	cout << "INFO: processVideo consuming frames. DONE" << endl;
+}
+
+/*
+	Continually read frames from the stream and place them in the queue.
+*/
+void processVideo(const string& videoFilename) {
+	cout << "INFO: processVideo starting." << endl;
+	std::chrono::high_resolution_clock timer;
+	using milisec = std::chrono::duration<float, std::milli>;
+	VideoReader vid(videoFilename);
+	ImageRes buff = vid.readFrame();
+	cout << "INFO: processVideo starting. DONE" << endl;
+	consumeBufferedFrames(vid, buff);
+
+	//We need a Mat to be mapped to the buffer we are using to read frames into.
+	Mat frame(vid.getHeight(), vid.getWidth(), CV_8UC3, buff.get());
+	while (keyboard != 'q' && keyboard != 27) {
+		int errorCode = 0;
+		if ((errorCode = vid.readFrame(buff)) < 0) {
+			vid = VideoReader(videoFilename);
+			consumeBufferedFrames(vid, buff);
+			continue;
+		}
+
+		cv::cvtColor(frame, frame, CV_BGR2RGB);
+		frameBuff.insertFrame(frame.clone());
+	}
+	keyboard = 'q';
+	cout << "INFO: processVideo exiting." << endl;
+	return;
+}
+
+#else
+
 void processVideo(string videoFileName, std::function<void(void)> onStart) {
 	//create the capture object
 	VideoCapture capture(videoFileName);
@@ -337,6 +393,8 @@ void processVideo(string videoFileName, std::function<void(void)> onStart) {
 	frameBuff.kill();
 	capture.release();
 }
+
+#endif
 
 /*Perform appropriate startup and shutdown of motor subsystem (handles GUI interface start & stop operations)*/
 void motorRunningHandler() {
